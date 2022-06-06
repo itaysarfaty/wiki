d9d30efc3485ac7638092becd21c6b1f0eaafe41
itaysarfaty <itaysarfaty@gmail.com>
2022-06-06 01:05:23 -0400

    New Web

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
 
+const openGithub = () => window.open("https://www.github.com/itaysarfaty");
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		
+		<div className="window">
+			<div className="content center">
+				<h1 className="heroText prevent-select">
+					<span id="bracket">[</span> EE-tie <span id="bracket">]</span>
+				</h1>
+				<div className="menu prevent-select">
+					<h2 className="menu-button" onClick={()=> console.log('portfolio')}>
+						portfolio
+					</h2>
+					<div className="dot"/>
+					<h2 className="menu-button" onClick={openGithub}>
+						github
+					</h2>
+					<div className="dot"/>
+					<h2 className="menu-button" onClick={()=> console.log('resume')}>
+						resume
+					</h2>
+				</div>
+			</div>
+			<div className="bottom-bar">
+				<div className="bottom-content center">
+					<div className="info-section vertical-center prevent-select">
+						<h2 className="info">
+							🎢 Product Oriented
+						</h2>
+						<h2 className="info">
+							🤝 Fullstack Developer
+						</h2>
+						<h2 className="info">
+							💻 B.S. in Computer Science
+						</h2>	
+					</div>
+					<h2 id='email'>
+						itaysarfaty@gmail.com
+					</h2>
+				</div>
+			</div>
+		</div>
+		
+	);
 }
+
 
 export default App;
